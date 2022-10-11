fe58b7859256eafde8fd9cac625f3798529054d2
AdaN <ada.n@users.noreply.github.com>
2022-10-11 10:50:55 +0200

    started something

---
+++ longest_string.js
@@ -0,0 +1,8 @@
+
+
+
+let sentenceSplit = 'The emergency scheme started after September\'s mini-budget, which spooked markets and drove up borrowing costs.'.split(' ');
+const longestString = (str) => {
+    let deepSplit = sentenceSplit.split('');
+    let longestString = 0
+}
