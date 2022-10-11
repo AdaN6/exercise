b6ae55ffa2ee2214408d1e1b4162eac9ace0d0ea
AdaN6 <a_disney_da@hotmail.com>
2022-10-11 16:25:49 +0200

    second attempt

--- longest_string.js
+++ longest_string.js
@@ -1,8 +1,21 @@
 
 
 
-let sentenceSplit = 'The emergency scheme started after September\'s mini-budget, which spooked markets and drove up borrowing costs.'.split(' ');
-const longestString = (str) => {
-    let deepSplit = sentenceSplit.split('');
-    let longestString = 0
+
+const longestWord = (str) => {
+    let deepSplit = str.split('');
+    let longestWord = 0;
+
+    for (let i=0; i < deepSplit.length; i++){
+        if (deepSplit[i].length > longestWord) {
+            longestWord = deepSplit[i].length;
+        }
+    }
+  
+    return longestWord;
 }
+
+console.log(longestWord(
+  "The emergency scheme started after September's mini-budget, which spooked markets and drove up borrowing costs."
+));
+
