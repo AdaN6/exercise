252c882b7324f2af3a5cbc5f334228724828188b
AdaN6 <a_disney_da@hotmail.com>
2022-10-12 09:55:47 +0200

    last commit

--- longest_string.js
+++ longest_string.js
@@ -2,20 +2,21 @@
 
 
 
-const longestWord = (str) => {
-    let deepSplit = str.split('');
+const findlongestWord = () => {
+    
+    let splitSentence =
+      "The emergency scheme started after September's mini-budget, which spooked markets and drove up borrowing costs.".split(' '
+      );
     let longestWord = 0;
 
-    for (let i=0; i < deepSplit.length; i++){
-        if (deepSplit[i].length > longestWord) {
-            longestWord = deepSplit[i].length;
-        }
+    for (let i = 0; i < splitSentence.length; i++) {
+      if (splitSentence[i].length > longestWord) {
+        longestWord = splitSentence[i].length;
+      }
     }
   
     return longestWord;
 }
 
-console.log(longestWord(
-  "The emergency scheme started after September's mini-budget, which spooked markets and drove up borrowing costs."
-));
+console.log(findlongestWord());
 
