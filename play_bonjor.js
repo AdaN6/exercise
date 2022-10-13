7bc57ccf6dc0b25dcd3006d9a79941d64887baf4
AdaN6 <a_disney_da@hotmail.com>
2022-10-13 20:37:56 +0200

    new methods of play bonjor

--- play_bonjor.js
+++ play_bonjor.js
@@ -13,5 +13,25 @@ function areYouPlayingBanjo(name) {
 console.log(areYouPlayingBanjo("Rachel"));
 
 function areYouPlayingBanjo(name) {
-  return name + (/^r/i.test(name) ? " plays " : " does not play ") + "banjo";
+return name + (/^r/i.test(name) ? " plays " : " does not play ") + "banjo";
 }
+
+function areYouPlayingBanjo(name) {
+
+  return name.charAt(0) === "R" || name.charAt(0) === "r"
+    ? `${name} plays banjo`
+    : `${name} does not play banjo`;
+
+}
+
+function areYouPlayingBanjo(name) {
+
+  if (name.charAt(0) === "R" || name.charAt(0) === "r")
+    return `${name} plays banjo`;
+  return `${name} does not play banjo`;
+}
+
+
+// if you only have one if no else, you can write this
+//return ((name.charAt(0) === "R" || name.charAt(0) === "r") && `${name} plays banjo`);
+
